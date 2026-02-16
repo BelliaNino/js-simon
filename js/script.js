a36a3e8e208366748586877d3bd24951a81bc58b
BelliaNino <bellianino1996@gmail.com>
2026-02-16 16:54:47 +0100

    add random numbs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
+console.log('it works');
+
 const countdownEl = document.getElementById('countdown')//countdown
 
 const instrEl = document.getElementById('instructions')//Istruzioni del gioco
 
-const numListEl = document.getElementById('number-list')//lista dei numeri casuali 
+const numListEl = document.getElementById('numbers-list')//lista dei numeri casuali 
 
 const formEl = document.getElementById('answers-form')//input per l'utente
 
 const btnEl = document.querySelector('.btn')//btn per verificare esito
 
 const esiText = document.getElementById('message')//mess esito
+
+
+function randomNum() {
+  let num = [];
+
+  for (let i = 0; i < 5; i++) {
+    const random = Math.floor(Math.random() * 50) + 1;
+    num.push(random);
+  }
+
+  return num;
+}
+
+const numbers = randomNum()
+numListEl.innerHTML = " "
+
+for (let i = 0; i < numbers.length; i++){
+  numListEl.innerHTML += `<li>${numbers[i]}</li>`
+}
+
 
 
 
