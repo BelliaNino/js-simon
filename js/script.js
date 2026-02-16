0092f4d0763bc4a7cb28070bbbd7bfe973613c2c
BelliaNino <bellianino1996@gmail.com>
2026-02-16 15:54:32 +0100

    add variables js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const countdownEl = document.getElementById('countdown')//countdown
+
+const instrEl = document.getElementById('instructions')//Istruzioni del gioco
+
+const numListEl = document.getElementById('number-list')//lista dei numeri casuali 
+
+const formEl = document.getElementById('answers-form')//input per l'utente
+
+const btnEl = document.querySelector('.btn')//btn per verificare esito
+
+const esiText = document.getElementById('message')//mess esito
+
+
+
+
+
+
